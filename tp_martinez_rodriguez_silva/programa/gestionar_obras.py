084402ef4e037eeb4999d412a663650a9c204b44
Stefania Silva <stefania.silva19@gmail.com>
2024-06-18 12:13:56 -0300

    estionarObra() esquema

--- tp_martinez_rodriguez_silva/programa/gestionar_obras.py
+++ tp_martinez_rodriguez_silva/programa/gestionar_obras.py
@@ -1,6 +1,6 @@
 import abc
 
-class GestionarObra(abc):
+class GestionarObra(abc.ABCMeta):
     #TODO: Revisar en detalle los argumentos pasados para verificarq ue estén correctos y los retornos
     
     
@@ -9,9 +9,25 @@ class GestionarObra(abc):
         pass
     
     @abc.abstractmethod
-    def conectar_db(self) -> bool:
+    def conectar_db(self, *args) -> bool:
         pass
     
     @abc.abstractmethod
-    def mapear_orm(self) -> object: 
+    def mapear_orm(self, **args) -> object: 
         pass
+    
+    @abc.abstractmethod
+    def limpiar_datos(drlf,*args) -> bool:
+        pass
+    
+    @abc.abstractmethod
+    def cargar_datos(self, *args) -> None:
+        pass
+    
+    @abc.abstractmethod
+    def nueva_obra(self, *args) -> None: 
+        pass 
+    
+    @abc.abstractmethod
+    def obtener_indicadores(self, *args) -> None: 
+        pass 
