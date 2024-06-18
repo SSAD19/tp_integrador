98835b2c8792acb3b0756ae7bf06f422cde02582
Stefania Silva <stefania.silva19@gmail.com>
2024-06-18 16:14:10 -0300

    cambios en main

--- tp_martinez_rodriguez_silva/programa/main.py
+++ tp_martinez_rodriguez_silva/programa/main.py
@@ -1,12 +1,11 @@
-from tp_martinez_rodriguez_silva.programa.importar_datos import *
-from modelo_orm import *
+from carga_datos import cargarDatosPrevios
+
 
 if __name__ == '__main__':
-  
-  
   # 1. importar dataset para crear las tablas de nuestro modelo_orm
-  data = importar_datos()
-  if data is False:
-    exit()
   
+  #Importo mis datos csv, paso path o uso el predeterminado
+  cargarDatosPrevios()
+
+
   
